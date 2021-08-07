0bee0b16895b2c350ae3a6615995a173b909f313
sonam1812 <sonampal832@gmail.com>
2021-08-07 20:13:15 +0530

    added parttime

---
+++ parttime.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+isPartTime=1
+isFulltime=2
+totalSalary=0
+empRatePerHr=20
+WorkingDays=20
+for (( day=1; day<=$WorkingDays; day++ ))
+do
+empCheck=$((RANDOM%3))
+case $empCheck in
+$isFullTime)
+empHrs=8
+;;
+$isPartTime)
+empHs=8
+;;
+*)
+empHrs=0
+;;
+esac
+salary=$(( $empHrs*$empRatePerHr ))
+totalSalary=$(( $totalSalary+$salary ))
+done
